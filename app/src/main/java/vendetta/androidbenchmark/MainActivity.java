5d56dfe1d1a2e671a90705f8ee99c2ee460dc5e0
vendettavn <vladnemes5@gmail.com>
2017-05-23 20:28:08 +0300

    Working on ScoreActivity. Updated UI

--- app/src/main/java/vendetta/androidbenchmark/MainActivity.java
+++ app/src/main/java/vendetta/androidbenchmark/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -27,37 +28,36 @@ public class MainActivity extends BaseActivity {
         Database.establishConnection(this);
     }
 
-    public static void updateScores(UserScores userScores, Context context){
+    public static void updateScores(UserScores userScores, Context context) {
         boolean isDifferent = false;
         scoresContainer.removeAllViewsInLayout();
-        for (final Map.Entry<String,String> entry: userScores.getScoreMap().entrySet()){
+        for (final Map.Entry<String, String> entry : userScores.getScoreMap().entrySet()) {
 
             LinearLayout currentLayout = new LinearLayout(context);
             currentLayout.setOrientation(LinearLayout.HORIZONTAL);
             if (isDifferent) currentLayout.setBackgroundColor(Color.parseColor("#e8eaf6"));
             else currentLayout.setBackgroundColor(Color.parseColor("#9fabf2"));
             isDifferent = !isDifferent;
-//            currentLayout.setPadding(5,2,5,2);
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
             currentLayout.setLayoutParams(lp);
 
             TextView benchNameTView = new TextView(context);
-            benchNameTView.setTextAppearance(context,android.R.style.TextAppearance_Large);
-            TableRow.LayoutParams tParams = new TableRow.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.MATCH_PARENT,1.0f);
+            benchNameTView.setTextAppearance(context, android.R.style.TextAppearance_Large);
+            TableRow.LayoutParams tParams = new TableRow.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f);
             benchNameTView.setLayoutParams(tParams);
             benchNameTView.setMinWidth(60);
             benchNameTView.setText(entry.getKey());
 
             TextView ScoreValTView = new TextView(context);
-            ScoreValTView.setTextAppearance(context,android.R.style.TextAppearance_Large);
-            tParams = new TableRow.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT,2f);
+            ScoreValTView.setTextAppearance(context, android.R.style.TextAppearance_Large);
+            tParams = new TableRow.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 2f);
             ScoreValTView.setLayoutParams(tParams);
             ScoreValTView.setGravity(Gravity.RIGHT);
             ScoreValTView.setText(entry.getValue());
             ScoreValTView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent benchActivityIntent = new Intent(v.getContext(),BenchmarkActivity.class);
+                    Intent benchActivityIntent = new Intent(v.getContext(), BenchmarkActivity.class);
                     benchActivityIntent.putExtra(BENCH_NAME, entry.getKey());
                     v.getContext().startActivity(benchActivityIntent);
                 }
@@ -67,6 +67,11 @@ public class MainActivity extends BaseActivity {
             currentLayout.addView(ScoreValTView);
             scoresContainer.addView(currentLayout);
         }
+    }
+
+    public void startFullBenchmark(View v) {
+        //TODO launh full benchmark suite here
+        Log.d("debug", "should start the benchmark suite");
     }
 
 }
